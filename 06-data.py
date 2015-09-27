19f2118f9af3263dd55566d8793ceaacbfbd0823
Lucy Park <me@lucypark.kr>
2015-09-27 10:56:03 +0900

    Add additional sample df

--- 06-data.py
+++ 06-data.py
@@ -4,11 +4,18 @@
 
 from numpy.random import randn, randint
 
+
 # a sample dataframe for testing purposes
 n = 1000
-sdf = pd.DataFrame({
+df_s = pd.DataFrame({
     'a': randn(n),
     'b': randn(n),
     'c': randint(10, 200, (n)),
     'y': 'x'
+})
+
+df_n = pd.DataFrame({
+    'movie': ['thg', 'thg', 'mol', 'mol', 'lob', 'lob'],
+    'rating': [3., 4., 5., np.nan, np.nan, np.nan],
+    'name': ['John', np.nan, 'N/A', 'Graham', np.nan, np.nan]
 })
