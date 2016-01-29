bade7337b8bc7bd05dec0c28bcf4ce0cf9fe7612
Lucy Park <me@lucypark.kr>
2016-01-29 14:05:58 +0900

    Fix pyspark startup error

--- 07-spark.py
+++ 07-spark.py
@@ -7,21 +7,22 @@
 SPARK_HOME = ''
 
 
-def run_spark(core=8, mem=1.5, driver_mem=1, executor_core=1,\
-              master='spark://127.0.0.1:7077'):
+def run_spark(core=8, mem=1.5, driver_mem=1, executor_core=1):
+
+    if not SPARK_HOME:
+        raise Exception(\
+            'Enter path for SPARK_HOME at the startup script `07-spark.py` to use spark')
 
     PYTHONPATH = ['%s/python' % SPARK_HOME,
                   '%s/python/lib/py4j-0.9-src.zip' % SPARK_HOME]
     PYTHONSTARTUP = '%s/python/pyspark/shell.py' % SPARK_HOME
-    PYSPARK_SUBMIT_ARGS = '--master=%s ' % master +\
-                          '--driver-memory %dg ' % driver_mem +\
+    PYSPARK_SUBMIT_ARGS = '--driver-memory %dg ' % driver_mem +\
                           '--executor-memory %dm ' % int(mem*1024) +\
                           '--executor-cores %s ' % executor_core +\
                           '--total-executor-cores=%s pyspark-shell' % core
 
-    #os.environ['PYSPARK_PYTHON']='/home/play/deploy/anaconda/bin/python'
     os.environ['SPARK_HOME'] = SPARK_HOME
     os.environ['PYSPARK_SUBMIT_ARGS'] = PYSPARK_SUBMIT_ARGS
     sys.path.extend(PYTHONPATH)
-    execfile(PYTHONSTARTUP, globals())
     print PYSPARK_SUBMIT_ARGS
+    execfile(PYTHONSTARTUP, globals())
